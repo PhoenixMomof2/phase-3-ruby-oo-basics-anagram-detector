3ddedcdf11e19bbd18d514b32d8f066e617dd36e
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2023-01-02 21:48:38 -0500

    assignment complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(array_of_words)
+    array_of_words.select {|n| n.split("").sort == @name.split("").sort} 
+    # need to map over the array of strings... 
+    # and return an array with the match or an empty array if no match
+  end
+end
